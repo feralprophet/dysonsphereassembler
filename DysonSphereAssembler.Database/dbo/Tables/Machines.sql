80d76d9aa50edbad1c4420c2ff0d4e403b28424d
Mark Salow <veudain@gmail.com>
2021-02-18 14:19:12 -0700

    Added Sql Project

---
+++ DysonSphereAssembler.Database/dbo/Tables/Machines.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Machines] (
+    [Id]                 INT          NOT NULL,
+    [Name]               VARCHAR (50) NOT NULL,
+    [MachineType]        INT          NOT NULL,
+    [ProductionModifier] DECIMAL (18) NOT NULL,
+    CONSTRAINT [PK_Machine] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
