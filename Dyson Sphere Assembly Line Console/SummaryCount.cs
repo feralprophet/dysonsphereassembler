c2472b5ac49d6269256f5754e89b9de1f877af87
Mark Salow <veudain@gmail.com>
2021-02-20 00:33:19 -0700

    Added a summary section. Added a menu. Move Import into a option for the menu.

---
+++ Dyson Sphere Assembly Line Console/SummaryCount.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Dyson_Sphere_Assembly_Line_Domain;
+
+namespace Dyson_Sphere_Assembly_Line_Console
+{
+    public class SummaryCount
+    {
+        public decimal SmelterCount { get; set; }
+        public decimal ChemicalPlant { get; set; }
+        public decimal Assembler { get; set; }
+        public decimal OilRefinery { get; set; }
+        public decimal ParticleCollider { get; set; }
+        public decimal MatrixLab { get; set; }
+        public Dictionary<string, decimal> BaseItemCount { get; set; } = new Dictionary<string, decimal>();
+        public Dictionary<MachineType, decimal> MachineCount { get; set; } = new Dictionary<MachineType, decimal>();
+    }
+}
