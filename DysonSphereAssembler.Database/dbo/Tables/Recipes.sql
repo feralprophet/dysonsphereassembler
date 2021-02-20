62135c4f60cbe12e6bdeb5786bf9793cf8e0654c
Mark Salow <veudain@gmail.com>
2021-02-19 17:05:44 -0700

    Update SQL Changes

--- DysonSphereAssembler.Database/dbo/Tables/Recipes.sql
+++ DysonSphereAssembler.Database/dbo/Tables/Recipes.sql
@@ -1,13 +1,15 @@
 ﻿CREATE TABLE [dbo].[Recipes] (
-    [Id]             INT          IDENTITY (1, 1) NOT NULL,
-    [Name]           VARCHAR (50) NOT NULL,
-    [ComponentId]    INT          NOT NULL,
-    [MachineType]    INT          NOT NULL,
-    [TimeToCreate]   DECIMAL (18) NOT NULL,
-    [NumberProduced] INT          NOT NULL,
-    [UseByDefault]   BIT          CONSTRAINT [DF_Recipes_UseByDefault] DEFAULT ((0)) NOT NULL,
+    [Id]             INT             IDENTITY (1, 1) NOT NULL,
+    [Name]           VARCHAR (50)    NOT NULL,
+    [ComponentId]    INT             NOT NULL,
+    [MachineType]    INT             NOT NULL,
+    [TimeToCreate]   DECIMAL (18, 1) NOT NULL,
+    [NumberProduced] INT             NOT NULL,
+    [UseByDefault]   BIT             CONSTRAINT [DF_Recipes_UseByDefault] DEFAULT ((0)) NOT NULL,
     CONSTRAINT [PK_Recipes] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
+
+
 
 
 
